4116ea6def70e80bce37158d43fe75c5d79c5354
Felicia Karlsson <felizia02@icloud.com>
2025-10-02 14:06:31 +0200

    header med logga ut funktion

---
+++ journal/src/app/header/header.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  username: string | null = '';
+
+  ngOnInit() {
+    this.username = localStorage.getItem('username');
+  }
+  
+constructor(private router: Router) {}
+
+  logout() {
+    localStorage.removeItem('username');
+    // ev. även ta bort JWT/token här
+    this.username = null;
+    this.router.navigate(['/login']);
+  }
+}
